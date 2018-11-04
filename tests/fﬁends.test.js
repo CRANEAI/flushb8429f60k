cc499c592b317598ab325f9e1cfb54bf301d385e
Elly <elly@mg.crane.ai>
2018-11-03 20:34:48 -0400

    added tests/fﬁends updated project details in new api

---
+++ tests/fﬁends.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - fﬁends UNIT TESTS
+ * Application Client
+ * flush
+*/
+
+/* the fﬁends view has a profile icon located at the mid-top left of the view which on click will trigger the profile() action */
+it("the fﬁends view has a profile icon located at the mid-top left of the view which on click will trigger the profile() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the fﬁends view has a profile icon located at the mid-top left of the view which on click will trigger the profile() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
